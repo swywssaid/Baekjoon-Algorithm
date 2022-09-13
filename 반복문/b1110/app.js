f9f24ec5cb7c6e90fcd7c54a504008a450f0d56a
swywssaid <94416699+swywssaid@users.noreply.github.com>
2022-09-14 00:23:25 +0900

    Step3: 반복문
    - 13

--- 반복문/b1110/app.js
+++ 반복문/b1110/app.js
@@ -1,20 +1,26 @@
 const fs = require("fs");
+let input = fs
+  .readFileSync(__dirname + "/input.txt") // boj: '/dev/stdin'
+  .toString()
+  .trim()
+  .split("\n");
+input = input[0];
 
-let input = Number(fs.readFileSync(__dirname + "/input.txt").toString());
-
-let num = input;
-let sum;
-let i = 0;
-
-while (true) {
-  i++;
-
-  sum = Math.floor(num / 10) + (num % 10);
-  num = (num % 10) * 10 + (sum % 10);
-
-  if (input === num) {
-    break;
+function solution() {
+  let origin = input.padStart(2, "0");
+  for (let i = 1; ; i++) {
+    input = input.padStart(2, "0");
+    let each = input.split("").map((x) => Number(x));
+    let sum = each
+      .reduce((a, c) => a + c, 0)
+      .toString()
+      .padStart(2, "0")
+      .split("");
+    input = each[1] + sum[1];
+    if (origin === input) {
+      console.log(i);
+      break;
+    }
   }
 }
-
-console.log(i);
+solution();
