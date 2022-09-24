882e7d67ad534ae1a8007dd1c2921a67477bc17e
swywssaid <94416699+swywssaid@users.noreply.github.com>
2022-09-25 00:49:35 +0900

    Step: 기본 수학1
    - 1712 best

--- 기본 수학1/b1712/better.js
+++ 기본 수학1/b1712/better.js
@@ -1,10 +1,9 @@
-const fs = require("fs");
-let input = fs
-  .readFileSync(__dirname + "/input.txt") // boj: "/dev/stdin"
+let input = require("fs")
+  .readFileSync("/dev/stdin") // boj: "/dev/stdin"
   .toString()
   .trim()
-  .split("\n");
-input = input[0].split(" ").map((x) => Number(x));
+  .split(" ")
+  .map((x) => Number(x));
 function solution(input) {
   let answer =
     input[1] < input[2] ? Math.floor(input[0] / (input[2] - input[1])) + 1 : -1;
